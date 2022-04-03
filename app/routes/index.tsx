6b4a61f57a9815227e4f0a1549855a94581f14b7
sammiepls <samantha.lhe@gmail.com>
2022-04-03 18:11:23 +0800

    Fix tailwind styling and add more home page styles

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,13 +3,25 @@ import { LinksFunction } from "@remix-run/server-runtime";
 
 export default function IndexRoute() {
   return (
-    <div className="h-screen flex">
-      <nav className="order-last">
+    <div className="h-screen flex flex-col p-4">
+      <nav className="order-last flex justify-between text-5xl">
         <a href="#">🤡</a>
         <a href="#">🔥</a>
       </nav>
-      <h1 className="text-white">Trash CAN not Trash cannot</h1>
-      <p>Ready for some HOT garbage? Tap the trash can</p>
+      <header>
+        <h1 className="text-white text-center text-5xl">
+          Trash CAN not Trash cannot
+        </h1>
+      </header>
+      <main className="flex-1 h-screen py-10">
+        <h1 className="text-9xl my-10 text-center">🗑</h1>
+
+        <div>
+          <p className="text-center">
+            Ready for some HOT garbage? Tap the trash can
+          </p>
+        </div>
+      </main>
     </div>
   );
 }
