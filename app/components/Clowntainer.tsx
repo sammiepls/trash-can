adecb837de33040de962629a02c6cf7c55de0762
sammiepls <samantha.lhe@gmail.com>
2022-04-04 21:48:13 +0800

    Create login component and add hot garbage toggle

--- app/components/Clowntainer.tsx
+++ app/components/Clowntainer.tsx
@@ -1,32 +1,11 @@
-import React from "react";
+import { Login } from "./Login";
 
 type Props = {};
 
 export const Clowntainer = (props: Props) => {
   return (
     <aside className="bg-white rounded-md p-4 mb-6 relative min-h-20">
-      <form className="flex flex-col items-center">
-        <h2 className="text-lg mb-4">Prove your clownery</h2>
-        <label>
-          <h3>Name</h3>
-          <input
-            className="bg-gray-200 mt-2 mb-4 py-2 px-4 rounded-md"
-            placeholder="Who are you"
-            type="text"
-          />
-        </label>
-        <label>
-          <h3>Password</h3>
-          <input
-            className="bg-gray-200 mt-2 mb-4 py-2 px-4 rounded-md"
-            placeholder="cedeatsass"
-            type="text"
-          />
-        </label>
-        <button className="bg-red-400 mt-2 py-2 px-10 rounded-md text-white text-center block">
-          🚀 Submit
-        </button>
-      </form>
+      <Login />
       <span className="speech" />
     </aside>
   );
