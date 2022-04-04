7ea0cb8666767fda7b86154f88339f00a48d7a06
sammiepls <samantha.lhe@gmail.com>
2022-04-04 22:11:33 +0800

    Add TrashForm component

--- app/components/Clowntainer.tsx
+++ app/components/Clowntainer.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import { Login } from "./Login";
+import { TrashForm } from "./TrashForm";
 
 type Props = {};
 
 export const Clowntainer = (props: Props) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
   return (
-    <aside className="bg-white rounded-md p-4 mb-6 relative min-h-20">
-      <Login />
+    <aside className="bg-white rounded-md p-8 mb-6 relative min-h-20">
+      {isAuthenticated ? <Login /> : <TrashForm />}
+
       <span className="speech" />
     </aside>
   );
--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -5,7 +5,7 @@ export const Login = () => {
       <label>
         <h3>Name</h3>
         <input
-          className="bg-gray-200 mt-2 mb-4 py-2 px-4 rounded-md"
+          className="bg-gray-200 mt-2 mb-4 py-2 px-4 rounded-md outline-none focus:border-red-200 border-4"
           placeholder="Who are you"
           type="text"
         />
@@ -13,7 +13,7 @@ export const Login = () => {
       <label>
         <h3>Password</h3>
         <input
-          className="bg-gray-200 mt-2 mb-4 py-2 px-4 rounded-md"
+          className="bg-gray-200 mt-2 mb-4 py-2 px-4 rounded-md outline-none focus:border-red-200 border-4"
           placeholder="cedeatsass"
           type="text"
         />
