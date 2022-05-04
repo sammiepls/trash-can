319c49b51d7696b20c1dd06fd9d1737ddebdf236
sammiepls <samantha.lhe@gmail.com>
2022-05-04 21:04:23 +0800

    Create ability to add new joke

---
+++ app/routes/joke/new.tsx
@@ -0,0 +1,94 @@
+import Layout from "~/components/Layout";
+import { Clowntainer } from "~/components/Clowntainer";
+import { ActionFunction, json } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import { useActionData } from "@remix-run/react";
+
+import { db } from "~/utils/db.server";
+
+type ActionData = {
+  formError?: string;
+  fieldErrors?: {
+    // name: string | undefined;
+    content: string | undefined;
+  };
+  fields?: {
+    // name: string;
+    content: string;
+  };
+};
+
+function validateJokeContent(content: string) {
+  if (content.length < 10) {
+    return `Were you even trying? That joke is too short`;
+  }
+}
+
+const badRequest = (data: ActionData) => json(data, { status: 400 });
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+  const content = form.get("content");
+  const clown = "Sammie";
+  // we do this type check to be extra sure and to make TypeScript happy
+  // we'll explore validation next!
+  if (typeof clown !== "string" || typeof content !== "string") {
+    return badRequest({
+      formError: `Form not submitted correctly.`,
+    });
+  }
+
+  const fieldErrors = {
+    // name: validateJokeName(name),
+    content: validateJokeContent(content),
+  };
+
+  const fields = { clown, content };
+
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return badRequest({ fieldErrors, fields });
+  }
+
+  const joke = await db.joke.create({ data: fields });
+  return redirect(`/joke/${joke.id}`);
+};
+
+export default function NewJokeRoute() {
+  const actionData = useActionData<ActionData>();
+
+  return (
+    <Layout>
+      <Clowntainer>
+        <form className="flex flex-col items-center" method="post">
+          <h2 className="text-lg mb-4">Clown mode activated</h2>
+          <label className="w-full">
+            <textarea
+              name="content"
+              defaultValue={actionData?.fields?.content}
+              aria-invalid={
+                Boolean(actionData?.fieldErrors?.content) || undefined
+              }
+              aria-errormessage={
+                actionData?.fieldErrors?.content ? "content-error" : undefined
+              }
+              rows={8}
+              placeholder="As if we need April to be fools"
+              className="bg-gray-200 mt-2 mb-4 py-2 px-4 rounded-md w-full resize-none outline-none focus:border-red-200 border-4"
+            />
+          </label>
+          {actionData?.fieldErrors?.content ? (
+            <p className="text-red-400" role="alert" id="content-error">
+              {actionData.fieldErrors.content}
+            </p>
+          ) : null}
+          <button
+            type="submit"
+            className="bg-red-400 mt-2 py-2 px-10 rounded-md text-white text-center block"
+          >
+            🚀 Submit
+          </button>
+        </form>
+      </Clowntainer>
+    </Layout>
+  );
+}
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -20,10 +20,9 @@ export const loader: LoaderFunction = async () => {
 export default function LoginRoute() {
   return (
     <Layout>
-      <aside className="bg-white rounded-md p-8 relative min-h-20 mt-auto">
-        <span className="speech" />
+      <Clowntainer>
         <Login />
-      </aside>
+      </Clowntainer>
     </Layout>
   );
 }
