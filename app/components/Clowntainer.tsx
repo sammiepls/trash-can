067bb7fa34b540191b1ce6a02d5f5ed2df57904c
sammiepls <samantha.lhe@gmail.com>
2022-05-04 20:40:52 +0800

    Adjust stylings

--- app/components/Clowntainer.tsx
+++ app/components/Clowntainer.tsx
@@ -6,7 +6,7 @@ type Props = {};
 
 export const Clowntainer = (props: Props) => {
   return (
-    <aside className="bg-white rounded-md p-8 mb-6 relative min-h-20">
+    <aside className="bg-white rounded-2xl p-8 mb-6 relative min-h-20 shadow-md">
       <Login />
       <span className="speech" />
     </aside>
