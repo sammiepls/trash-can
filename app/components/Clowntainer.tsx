2af711a2efb6e34358bde8b2687595e4abe218bc
sammiepls <samantha.lhe@gmail.com>
2022-05-04 21:04:09 +0800

    Sort out components

--- app/components/Clowntainer.tsx
+++ app/components/Clowntainer.tsx
@@ -1,13 +1,7 @@
-import { useState } from "react";
-import { Login } from "./Login";
-import { TrashForm } from "./TrashForm";
-
-type Props = {};
-
-export const Clowntainer = (props: Props) => {
+export const Clowntainer = ({ children }: { children: React.ReactNode }) => {
   return (
-    <aside className="bg-white rounded-2xl p-8 mb-6 relative min-h-20 shadow-md">
-      <Login />
+    <aside className="bg-white rounded-2xl p-8  relative min-h-20 shadow-md mt-auto">
+      {children}
       <span className="speech" />
     </aside>
   );
