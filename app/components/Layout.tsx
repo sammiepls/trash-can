ff12a6c2cb28cd05e8d9c3e2a732690ce8f34f36
sammiepls <samantha.lhe@gmail.com>
2022-05-04 20:13:13 +0800

    Only load jokes on click

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,38 +1,20 @@
 import { Link, useFetcher } from "@remix-run/react";
-import { ReactChildren, useEffect, useState } from "react";
-
-import { db } from "~/utils/db.server";
-import { useLoaderData } from "@remix-run/react";
-import { json, LoaderFunction } from "@remix-run/node";
-import type { Joke } from "@prisma/client";
-
-// type LoaderData = { jokes: Array<Joke> };
-
-export const loader: LoaderFunction = async () => {
-  const data: LoaderData = {
-    jokes: await db.joke.findMany(),
-  };
-  return json(data);
-};
-
-function pickRandomJoke(jokes: Joke[]) {
-  return jokes[Math.floor(Math.random() * jokes.length)]?.id;
-}
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const [jokeId, setJokeId] = useState<string>();
   const fetcher = useFetcher();
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    fetcher.load("/jokes");
-  }, []);
+  const loadRandomJoke = async () => {
+    await fetcher.load("/jokes");
+  };
 
   useEffect(() => {
     if (fetcher.data) {
-      const randomJoke = pickRandomJoke(fetcher.data.jokes);
-      setJokeId(randomJoke || fetcher.data.jokes[0].id);
+      navigate(`/joke/${fetcher.data.joke.id}`);
     }
-  }, [fetcher.data]);
+  }, [fetcher.data, navigate]);
 
   return (
     <div className="h-screen flex flex-col p-4">
@@ -48,16 +30,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         </h1>
       </header>
       <main className="flex-1 h-screen py-10 flex flex-col">
-        <Link
-          to={`/joke/${jokeId}`}
-          reloadDocument
+        <button
+          onClick={loadRandomJoke}
           className="text-9xl my-10 mx-auto text-center relative"
         >
-          <button className="text-9xl my-10 mx-auto text-center relative">
-            🗑
-            {<span className="absolute left-0 bottom-8">🔥</span>}
-          </button>
-        </Link>
+          🗑
+          {<span className="absolute left-0 bottom-8">🔥</span>}
+        </button>
         {children}
       </main>
     </div>
