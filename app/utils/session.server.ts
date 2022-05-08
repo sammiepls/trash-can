dcfd031e5c611a4e82a3a3a140b099d6feef58b5
sammiepls <samantha.lhe@gmail.com>
2022-05-08 20:43:02 +0800

    Create session storage for user login

---
+++ app/utils/session.server.ts
@@ -0,0 +1,54 @@
+import bcrypt from "bcryptjs";
+import { createCookieSessionStorage, redirect } from "@remix-run/node";
+
+import { db } from "./db.server";
+
+type LoginForm = {
+  name: string;
+  password: string;
+};
+
+export async function login({ name, password }: LoginForm) {
+  const clown = await db.clown.findUnique({
+    where: { name },
+  });
+
+  if (!clown) return null;
+
+  const isCorrectPassword = await bcrypt.compare(password, clown.passwordHash);
+
+  if (!isCorrectPassword) return null;
+
+  return {
+    id: clown.id,
+    name,
+  };
+}
+
+const sessionSecret = process.env.SESSION_SECRET;
+
+if (!sessionSecret) {
+  throw new Error("SESSION_SECRET must be set");
+}
+
+const storage = createCookieSessionStorage({
+  cookie: {
+    name: "CLOWN_session",
+    secure: process.env.NODE_ENV === "production",
+    secrets: [sessionSecret],
+    sameSite: "lax",
+    path: "/",
+    maxAge: 60 * 60 * 24 * 30,
+    httpOnly: true,
+  },
+});
+
+export async function createUserSession(clownId: string, redirectTo: string) {
+  const session = await storage.getSession();
+  session.set("clownId", clownId);
+  return redirect(redirectTo, {
+    headers: {
+      "Set-Cookie": await storage.commitSession(session),
+    },
+  });
+}
